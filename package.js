c8c0ccb966caffe0bc2f582ec27fe688142e8edb
waitingkuo <waitingkuo0527@gmail.com>
2014-08-23 21:16:20 +0800

    update package name

--- package.js
+++ package.js
@@ -1,9 +1,9 @@
 Package.describe({
   summary: 'A collection of HTML element and attribute style-normalizations',
-  version: '0.0.3'
+  version: '0.1.0'
 });
 
 Package.onUse(function (api) {
-  api.versionsFrom('METEOR-CORE@0.9.0-rc9');
+  api.versionsFrom('METEOR-CORE@0.9.0-rc12');
   api.addFiles(['normalize.css/normalize.css'], 'client');
 });
